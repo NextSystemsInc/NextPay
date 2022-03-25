433f66fd13cfdd9b35ace37bf75d2e43cdd4031e
zeankundev <zeanfender11@gmail.com>
2022-03-25 07:17:40 +0700

    Updated about.tsx and package.json

--- screens/About.tsx
+++ screens/About.tsx
@@ -1,5 +1,7 @@
-import React, { memo } from "react";
-import { StyleSheet, Text, TouchableOpacity, View, Image } from "react-native";
+import React, { memo, useState, useEffect} from "react";
+import uuid from 'react-native-uuid';
+import Clipboard from 'expo-clipboard';
+import { StyleSheet, Text, TouchableOpacity, View, Image, Linking, Touchable, Share } from "react-native";
 import {
     Background3 as Background,
     BackButton,
@@ -7,6 +9,8 @@ import {
     NumberKeyboard,
     Paragraph,
   } from "../components";
+
+import { Snackbar } from "react-native-paper";
     import { Navigation } from "../types";
 import DeviceInfo from "react-native-device-info";
 
@@ -14,7 +18,21 @@ import DeviceInfo from "react-native-device-info";
         navigation: Navigation;
     };
 
-const AboutScreen = ({ navigation }:Props) => {
+const AboutScreen = ({ navigation }: Props) => {
+    const [visible, setVisible] = useState(false);
+
+    const onToggleSnackBar = () => setVisible(!visible);
+
+    const onDismissSnackBar = () => setVisible(false);
+
+    const copyUUID = async () => {
+        // copy uuid.v4() string to clipboard without await
+        const uuidString = uuid.v4().toString();
+        // copy uuidString to clipboard
+        await Clipboard.setString(uuidString);
+        onToggleSnackBar();
+    };
+
     return (
         <Background>
             <Image 
@@ -30,7 +48,22 @@ const AboutScreen = ({ navigation }:Props) => {
             <BackButton goBack={() => navigation.navigate("Settings")} />
             <Header>About NextPay</Header>
             <Paragraph>Made by zeankun.dev</Paragraph>
-            <Paragraph>This is build 1.0.0</Paragraph>
+            <TouchableOpacity onPress={() => copyUUID()}>
+                <Paragraph>This is build 1.1.0</Paragraph>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => Linking.openURL('https://github.com/zeankundev/NextPay')}>
+                <Paragraph>GitHub repository here</Paragraph>
+            </TouchableOpacity>
+            <Snackbar
+                visible={visible}
+                onDismiss={onDismissSnackBar}
+                action={{
+                    label: "OK",
+                    onPress: () => {
+                        onDismissSnackBar();
+                    },
+                }}
+            >UUID has been copied!</Snackbar>
         </Background>
     );
 };
